b55548cfcf20daf4b430fd1bf8e76bb3e5db101c
tuxi <code@objc.com>
2019-05-17 18:40:01 +0800

    Test ner demo.

---
+++ 03/04_ner/grammar/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/17 5:48 PM
+# @Author  : xiaoyuan
+# @Site    : https://github.com/tuxi
+# @File    : __init__.py.py
+# @Software: PyCharm
